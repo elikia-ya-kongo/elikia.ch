94483903b0bc559078a684eeab392f7a57d79387
Lukas Neukom <6952622+lukasneukom@users.noreply.github.com>
2025-01-22 23:15:12 +0100

    Prepare default layout

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,6 @@
 export default function(eleventyConfig) {
   eleventyConfig.setInputDirectory("pages");
+  eleventyConfig.setInputDirectory("../layouts");
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("fonts");
---
+++ pages/mithelfen.md
@@ -0,0 +1,7 @@
+---
+layout: default
+title: Mithelfen
+---
+
+# {{ title }}
+Hilf mit, das Leben von Kindern und Jugendlichen in Not zu verbessern!
